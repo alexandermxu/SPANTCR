9d749af097a7ae9239a89b3801b6770b280ae4e0
alexandermxu <alex.m.xu@gmail.com>
2021-11-01 17:05:04 -0700

    4th commit

---
+++ R/EntropyAnalysisFunctions.R
@@ -0,0 +1,54 @@
+#' Checks entropy contributions for all TCRs expressing a specific motif (searchterms) in a specific range (targetrange)
+#'
+#' @param base CDR3Breakdown object
+#' @param searchterms Vector of k-mers to analyze matching the length analyzed in base
+#' @param targetrange Two element vector describing the lower and upper bound of CDR3 region analyzed (from 0-1)
+#' @param ticks Vector of ticks to analyze, FineBinTicks for single CDR3, FineBinTicksPaired for paired CDR3s
+#'
+#' @return data.table object describing all k-mers contribution to entropy
+#' @export
+SearchIterator <- function(base, searchterms, targetrange, ticks=FineBinTicks)
+{
+  BaseEntropy <- base@Output[, mean(WeightEntropy), by=Tick]$V1
+  FullEntropyList <- data.table::data.table(Location=ticks, Entropy=BaseEntropy, Source="Base", N=nrow(base@CloneData), Range=paste0(targetrange, collapse="-"))
+  for(n in searchterms)
+  {
+    HitList <- unique(base@Output[Tick>targetrange[1] & Tick<targetrange[2] & Window==n]$Origin)
+    SearchData <- base@Output[Origin %in% HitList]
+    UniqueWindows <- sapply(ticks, function(tick) unique(SearchData[Tick==tick]$Window))
+    ProbabilityData <- SearchData[, list(Probability=sum(WeightProbability)), by=.(Tick, Window)]
+    EntropyData <- ProbabilityData[, list(Entropy=sum(sapply(.SD/sum(.SD), function(x) -x*log(x)))), by=Tick, .SDcols="Probability"]
+    FullEntropyList <- rbind(FullEntropyList, data.table::data.table(Location=ticks, Entropy=EntropyData$Entropy, Source=I(n), N=length(HitList), Range=paste0(targetrange, collapse="-")))
+  }
+  FullEntropyList
+}
+
+
+#' Checks entropy contributions for each major motif in each range window
+#'
+#' @param Data CDR3Breakdown object
+#' @param Searches List of vectors containing search ranges, default SearchBoxes for single CDR3 SearchBoxesPaired for paired
+#' @param Limit Minimum frequency of k-mers to analyze in each bin, default 0.03
+#' @param ticksused Vector of ticks to analyze, FineBinTicks for single CDR3, FineBinTicksPaired for paired CDR3s
+#'
+#' @return Summarized result of function SearchIterator called on each range in Searches
+#' @export
+EntropyScan <- function(Data, Searches, Limit=0.03, ticksused=FineBinTicks)
+{
+  EntropySummary <- data.table::data.table()
+  EntropyRaw <- data.table::data.table()
+  if(!is.null(Data)){
+    for(range in Searches)
+    {
+      hitsinrange <- sum(table(Data@Output[Tick>range[1] & Tick<range[2]]$Window))
+      targets <- which(table(Data@Output[Tick>range[1] & Tick<range[2]]$Window)>(hitsinrange*Limit))
+      output <- SearchIterator(Data, targets %>% names, range, ticksused)
+      NewEntropySummary <- output[, .(Average=mean(Entropy), Count=unique(N), Range=unique(Range)), by=Source]
+      EntropySummary <- rbind(EntropySummary, NewEntropySummary)
+      EntropyRaw <- rbind(EntropyRaw, output)
+    }
+    set(EntropySummary, j="DeltaAverage", value=max(EntropySummary$Average)-EntropySummary$Average)
+  }
+  list(EntropySummary, EntropyRaw)
+}
+
---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Modified BLOSUM62 matrix.
+#'
+#' A matrix containing thresholded BLOSUM values to calculate distance between k-mers.
+#'
+#' @format A matrix with 22 rows and 22 columns:
+#'
+"BLOSUMCappedDiff"
+
+#' Amino acid parameter matrix.
+#'
+#' A matrix containing properties of amino acids.
+#'
+#' @format A matrix with 22 rows and 6 columns:
+#'
+"AminoAcidFilter"
+
+#' Vector of tick centroids 1-100 to use with single CDR3 genes for distance comparisons.
+#'
+#' A vector from 0.005 to 0.995.
+#'
+#' @format A vector of length 100:
+#'
+"FineBinTicks"
+
+#' Vector of tick centroids 1-200 to use with paired CDR3 for distance comparisons.
+#'
+#' A vector from 0.005 to 1.995.
+#'
+#' @format A vector of length 200:
+#'
+"FineBinTicksPaired"
